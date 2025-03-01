ec4ade051fbdc394558603904687371ee731c764
Parthiv Kativarapu <parthiv.kativarapu@ideas.com>
2025-03-01 22:29:41 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+<p align="center">
+  <img alt="Cat Gif" src="https://github.com/parthiv13/treat/blob/master/assets/brand.jpeg">
+</p>
+
 # PRaiseMeSenpai
 Because every PR deserves recognition
+
+This is a simple Github Action which comments on your PRs with a gif of my friends pic as a reward for pushing some code.
+
+This is just a novelty action, but feel free to use it. If you'd like to contribute then just open a PR.
+
+## Usage
+
+```yaml          
+name: Treat for your PR 🍬
+
+on:
+  pull_request_target:
+    types:
+      - opened
+      - reopened
+
+jobs:
+  aTreatForCreatingThePullRequest:
+    name: A treat for your effort!
+    runs-on: ubuntu-latest
+    steps:
+      - uses: parthiv13/treat@final
+        with:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const gifs = [
+    'https://github.com/parthiv13/treat/blob/master/assets/1.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/2.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/3.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/4.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/5.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/6.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/7.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/8.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/9.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/10.gif',
+    'https://github.com/parthiv13/treat/blob/master/assets/11.gif'
+];
+
+async function run() {
+    try {
+        const token = core.getInput("github_token");
+
+        const pics = gifs;
+        // Select a random image
+        const randomImage = pics[Math.floor(Math.random() * pics.length)];
+        const prNumber = github.context.payload.pull_request?.number;
+
+        if (!prNumber) {
+            core.info("No pull request found, skipping.");
+            return;
+        }
+
+        const commentBody = `![Random Pic](${randomImage})`;
+
+        const octokit = github.getOctokit(token);
+        await octokit.rest.issues.createComment({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: prNumber,
+            body: commentBody
+        });
+
+        core.info(`Comment posted on PR #${prNumber} with image: ${randomImage}`);
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run()
